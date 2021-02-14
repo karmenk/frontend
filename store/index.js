2368bc560ba52e79a44dc40461f0a8f139e6ed2d
Karmen Kukk <karmenkukk91@gmail.com>
2021-02-14 18:52:41 +0200

    Test

--- store/index.js
+++ store/index.js
@@ -3,31 +3,27 @@ import { setAuthToken, resetAuthToken } from '~/utils/auth'
 
 export const actions = {
   nuxtServerInit ({ dispatch }, { req }) {
-    console.log('nuxtServerInit')
-    // console.log(req)
     return new Promise((resolve, reject) => {
-      if (req) {
-        console.log('is req')
-        const cookies = cookie.parse(req.headers.cookie || '')
-        if (Object.prototype.hasOwnProperty.call(cookies, 'x-access-token')) {
-          setAuthToken(cookies['x-access-token'])
-          console.log('dispatching')
-          dispatch('auth/fetch')
-            .then((result) => {
-              resolve(true)
-            })
-            .catch((error) => {
-              console.log('Provided token is invalid:', error)
-              resetAuthToken()
-              resolve(false)
-            })
-        } else {
-          resetAuthToken()
-          resolve(false)
-        }
+      // if (req) {
+      const cookies = cookie.parse(req.headers.cookie || '')
+      if (Object.prototype.hasOwnProperty.call(cookies, 'x-access-token')) {
+        setAuthToken(cookies['x-access-token'])
+        dispatch('auth/fetch')
+          .then((result) => {
+            resolve(true)
+          })
+          .catch((error) => {
+            console.log('Provided token is invalid:', error)
+            resetAuthToken()
+            resolve(false)
+          })
       } else {
-        resolve(true)
+        resetAuthToken()
+        resolve(false)
       }
+      // } else {
+      //   resolve(true)
+      // }
     })
   }
 }
