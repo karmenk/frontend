e7e5d3e3bb6270b7c1b57cc5b9c4c3f1c0078c30
Karmen Kukk <karmenkukk91@gmail.com>
2021-02-14 15:36:00 +0200

    Error page

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <p>Error details: {{ error.message }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+
+<style>
+</style>
